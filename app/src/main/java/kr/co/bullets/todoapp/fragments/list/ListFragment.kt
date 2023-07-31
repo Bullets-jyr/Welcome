65a78fe2a8946e289d9fa652ec7526168d7b9bde
Bullets-jyr <jyr@bullets.co.kr>
2023-07-31 17:50:22 +0900

    6. Create Navigation Graph

---
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
@@ -0,0 +1,19 @@
+package kr.co.bullets.todoapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.bullets.todoapp.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+}
