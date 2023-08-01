d590914aa48be19ca1fe4e94e11d2097a055a1ed
Bullets-jyr <jyr@bullets.co.kr>
2023-08-01 23:35:49 +0900

    8. Design AddFragment Layout

--- app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import kr.co.bullets.todoapp.R
+import kr.co.bullets.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
     }
 }
