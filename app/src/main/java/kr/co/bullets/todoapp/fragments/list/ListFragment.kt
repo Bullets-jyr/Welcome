71463e666250a33ce2151481590e694bcbf31458
Bullets-jyr <jyr@bullets.co.kr>
2023-08-05 18:10:42 +0900

    10. Create Menu for ListFragment

--- app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package kr.co.bullets.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import kr.co.bullets.todoapp.R
 import kr.co.bullets.todoapp.databinding.FragmentListBinding
@@ -27,6 +25,13 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set Menu
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
