5c6425b89412fd61e3e209885de19c4b260b4be9
Bullets-jyr <jyr@bullets.co.kr>
2023-08-05 17:33:05 +0900

    9. Design UpdateFragment Layout

--- app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
@@ -23,6 +23,10 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        binding.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
         return binding.root
     }
 }
