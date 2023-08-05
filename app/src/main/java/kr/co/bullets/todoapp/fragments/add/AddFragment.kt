327c1219ccb7b0653666c2bfa45821958cf7c8b2
Bullets-jyr <jyr@bullets.co.kr>
2023-08-05 18:19:07 +0900

    11. Create Menu for AddFragment

--- app/src/main/java/kr/co/bullets/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/add/AddFragment.kt
@@ -1,19 +1,27 @@
 package kr.co.bullets.todoapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import kr.co.bullets.todoapp.R
+import kr.co.bullets.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+    private lateinit var binding: FragmentAddBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }
