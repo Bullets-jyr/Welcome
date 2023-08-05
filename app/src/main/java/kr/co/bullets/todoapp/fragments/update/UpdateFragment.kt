a0f419d91fd36c3e49c0a28200bedc89138a3152
Bullets-jyr <jyr@bullets.co.kr>
2023-08-05 18:29:24 +0900

    12. Create Menu for UpdateFragment

--- app/src/main/java/kr/co/bullets/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/update/UpdateFragment.kt
@@ -1,19 +1,27 @@
 package kr.co.bullets.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import kr.co.bullets.todoapp.R
+import kr.co.bullets.todoapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+    private lateinit var binding: FragmentUpdateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        binding = FragmentUpdateBinding.inflate(inflater, container, false)
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 }
