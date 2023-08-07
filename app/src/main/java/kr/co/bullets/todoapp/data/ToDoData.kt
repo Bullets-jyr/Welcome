296193be1efe36bac53b91f3f82b6663bda9da51
Bullets-jyr <jyr@bullets.co.kr>
2023-08-07 17:11:55 +0900

    17. Create Entity (ToDoData) - Model class

---
+++ app/src/main/java/kr/co/bullets/todoapp/data/ToDoData.kt
@@ -0,0 +1,13 @@
+package kr.co.bullets.todoapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var priority: Priority,
+    var description: String,
+)
