6fb4e1adfd8345df130b6001074d3c3595e90784
Bullets-jyr <jyr@bullets.co.kr>
2023-08-10 04:36:47 +0900

    23. Insert Data to Database

--- app/src/main/java/kr/co/bullets/todoapp/fragments/add/AddFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/add/AddFragment.kt
@@ -1,13 +1,21 @@
 package kr.co.bullets.todoapp.fragments.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import kr.co.bullets.todoapp.R
+import kr.co.bullets.todoapp.data.models.Priority
+import kr.co.bullets.todoapp.data.models.ToDoData
+import kr.co.bullets.todoapp.data.viewmodel.ToDoViewModel
 import kr.co.bullets.todoapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
     private lateinit var binding: FragmentAddBinding
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +31,51 @@ class AddFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_add) {
+            insertDataToDb()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDb() {
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = verifyDataFromUser(mTitle, mDescription)
+        if (validation) {
+            // Insert Data to Database
+            val newData = ToDoData(
+                0,
+                mTitle,
+                parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun verifyDataFromUser(title: String, description: String): Boolean {
+        return if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description)) {
+            false
+        } else {
+            !(title.isEmpty() || description.isEmpty())
+        }
+    }
+
+    private fun parsePriority(priority: String): Priority {
+        return when (priority) {
+            "High Priority" -> { Priority.HIGH }
+            "Medium Priority" -> { Priority.MEDIUM }
+            "Low Priority" -> { Priority.LOW }
+            else -> Priority.LOW
+        }
     }
 }
