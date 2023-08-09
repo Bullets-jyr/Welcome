c5d7076982d4fe75afa12a859a8ac98d214bc18a
Bullets-jyr <jyr@bullets.co.kr>
2023-08-09 18:11:10 +0900

    20. Create Type Converter (Converter)

---
+++ app/src/main/java/kr/co/bullets/todoapp/data/Converter.kt
@@ -0,0 +1,16 @@
+package kr.co.bullets.todoapp.data
+
+import androidx.room.TypeConverter
+
+class Converter {
+
+    @TypeConverter
+    fun fromPriority(priority: Priority): String {
+        return priority.name
+    }
+
+    @TypeConverter
+    fun toPriority(priority: String): Priority {
+        return Priority.valueOf(priority)
+    }
+}
