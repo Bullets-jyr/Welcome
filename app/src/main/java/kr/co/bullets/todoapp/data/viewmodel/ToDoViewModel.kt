2252935bd5de8079cf18fb726c49afe75327cb64
Bullets-jyr <jyr@bullets.co.kr>
2023-08-10 04:16:31 +0900

    22. Create ViewModel (ToDoViewModel)

---
+++ app/src/main/java/kr/co/bullets/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,30 @@
+package kr.co.bullets.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.co.bullets.todoapp.data.ToDoDatabase
+import kr.co.bullets.todoapp.data.models.ToDoData
+import kr.co.bullets.todoapp.data.repository.ToDoRepository
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
