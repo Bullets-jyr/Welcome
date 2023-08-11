c39ed3928e1e3de61cded98cf291f35afe774438
Bullets-jyr <jyr@bullets.co.kr>
2023-08-11 17:41:21 +0900

    29. Display Data in Recyclerview

--- app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/list/ListFragment.kt
@@ -3,12 +3,20 @@ package kr.co.bullets.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.bullets.todoapp.R
+import kr.co.bullets.todoapp.data.viewmodel.ToDoViewModel
 import kr.co.bullets.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
+    private val adapter: ListAdapter by lazy {
+        ListAdapter()
+    }
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +24,13 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
