c5a75b82292c4e7aa394f5ba878939592c965082
Bullets-jyr <jyr@bullets.co.kr>
2023-08-12 12:20:08 +0900

    31. Update Selected Item

--- app/src/main/java/kr/co/bullets/todoapp/data/ToDoDao.kt
+++ app/src/main/java/kr/co/bullets/todoapp/data/ToDoDao.kt
@@ -1,10 +1,7 @@
 package kr.co.bullets.todoapp.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import kr.co.bullets.todoapp.data.models.ToDoData
 
 @Dao
@@ -15,4 +12,7 @@ interface ToDoDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertData(toDoData: ToDoData)
+
+    @Update
+    suspend fun updateData(toDoData: ToDoData)
 }
--- app/src/main/java/kr/co/bullets/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/kr/co/bullets/todoapp/fragments/update/UpdateFragment.kt
@@ -2,11 +2,15 @@ package kr.co.bullets.todoapp.fragments.update
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kr.co.bullets.todoapp.R
 import kr.co.bullets.todoapp.data.models.Priority
+import kr.co.bullets.todoapp.data.models.ToDoData
+import kr.co.bullets.todoapp.data.viewmodel.ToDoViewModel
 import kr.co.bullets.todoapp.databinding.FragmentUpdateBinding
 import kr.co.bullets.todoapp.fragments.SharedViewModel
 
@@ -14,6 +18,7 @@ class UpdateFragment : Fragment() {
     private lateinit var binding: FragmentUpdateBinding
     private val args by navArgs<UpdateFragmentArgs>()
     private val mSharedViewModel: SharedViewModel by viewModels()
+    private val mToDoViewModel: ToDoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +31,7 @@ class UpdateFragment : Fragment() {
 
         binding.currentTitleEt.setText(args.currentItem.title)
         binding.currentDescriptionEt.setText(args.currentItem.description)
-        binding.currentPrioritiesSpinner.setSelection(parsePriority(args.currentItem.priority))
+        binding.currentPrioritiesSpinner.setSelection(mSharedViewModel.parsePriorityToInt(args.currentItem.priority))
         binding.currentPrioritiesSpinner.onItemSelectedListener = mSharedViewModel.listener
 
         return binding.root
@@ -36,11 +41,33 @@ class UpdateFragment : Fragment() {
         inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
-    private fun parsePriority(priority: Priority): Int {
-        return when (priority) {
-            Priority.HIGH -> 0
-            Priority.MEDIUM -> 1
-            Priority.LOW -> 2
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_save) {
+            updateItem()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun updateItem() {
+        val title = binding.currentTitleEt.text.toString()
+        val description = binding.currentDescriptionEt.text.toString()
+        val getPriority = binding.currentPrioritiesSpinner.selectedItem.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(title, description)
+        if (validation) {
+            // Update Current Item
+            val updatedItem = ToDoData(
+                args.currentItem.id,
+                title,
+                mSharedViewModel.parsePriority(getPriority),
+                description
+            )
+            mToDoViewModel.updateData(updatedItem)
+            Toast.makeText(requireContext(), "Successfully updated!", Toast.LENGTH_SHORT).show()
+            // Navigate back
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show()
         }
     }
 }
